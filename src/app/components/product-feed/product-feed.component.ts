7695d6798dcb57ee792558f80ff57b3b094dafe5
Julian Giebel <j.giebel@netrocks.info>
2020-11-23 10:46:24 +0100

    Fix choosing mockdata for the product feed therough it's filter Imlement days until MHD calculation in product feeds

--- src/app/components/product-feed/product-feed.component.ts
+++ src/app/components/product-feed/product-feed.component.ts
@@ -31,19 +31,23 @@ export class ProductFeedComponent implements OnInit {
     new Produkt("1", "1", "kg", 10, 100, new Date(2021, 1, 13), "Kartoffeln", 0.6, 2),
   ];
 
-  products: Produkt[];
+  products = () => this.filter == FeedFilterType.DISTANCE ? this.DistanceMockData : this.TimeMockData;
 
   constructor() {
 
-    switch (this.filter) {
-      case FeedFilterType.DISTANCE:
-        this.products = this.DistanceMockData;
-      break;
-      case FeedFilterType.TIME:
-        this.products = this.TimeMockData;
-      break;
-    }
+    // switch (this.filter) {
+    //   case FeedFilterType.DISTANCE:
+    //     this.products = this.DistanceMockData;
+    //   break;
+    //   case FeedFilterType.TIME:
+    //     this.products = this.TimeMockData;
+    //   break;
+    // }
 
+  }
+
+  public TimeUntilMHD(mhd: Date): number {
+    return Math.trunc((mhd.valueOf() - new Date().valueOf()) /(1000*60*60*24));
   }
 
   ngOnInit(): void {
